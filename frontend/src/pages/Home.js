68d8c5c66a188a0ba3ea624fab9365c3ce2798a3
Fernanda <fernandadants@hotmail.com>
2024-04-13 11:45:07 -0300

    configurando e atualizando front

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+function Home() {
+    return (
+        <>
+            <Header />
+            <div>
+                <h1>ZoomAI</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home;
