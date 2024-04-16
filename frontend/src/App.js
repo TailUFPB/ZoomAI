ecd4e98f33d698174cbb1406615ba868d26de0e3
Fernanda <fernandadants@hotmail.com>
2024-04-15 23:23:32 -0300

    adicionando efeito de zoom in e zoom out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Project from './pages/Zoom';
 
 function App() {
    return (
@@ -8,6 +9,7 @@ function App() {
          <BrowserRouter>
             <Routes>
                <Route path="/" element={<Home />} />
+               <Route path="/tti" element={<Project />} />
             </Routes>
          </BrowserRouter>
       </div>
