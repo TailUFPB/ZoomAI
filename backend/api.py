649d4f0c9de914f2bda80e41d0f56d79d4ddec11
Allejandro <allejandro.empresarial@gmail.com>
2024-04-17 19:09:23 -0300

    generate_images function

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI
 import sys
 import os
 
@@ -6,3 +7,34 @@ sys.path.append(parent_dir)
 sys.path.append(os.path.join(parent_dir, 'models'))
 
 from models.generate_images import Generator
+
+#adding cors headers
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# adding cors urls
+origins = [
+    'http://localhost:3000'
+]
+
+# add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins, 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
+
+@app.get('/create')
+async def create_infinite_zoom(prompt, model_id, negative_prompt, 
+        num_outpainting_steps, guidance_scale, num_inference_steps,
+        custom_init_image):
+    g = await Generator()
+    better_prompt = await g.gpt_prompt_create(prompt)
+    imgs = await g.sd_generate_image(model_id, better_prompt, negative_prompt, num_outpainting_steps,
+                guidance_scale, num_inference_steps,custom_init_image)
+    return {"status" : "ok", "data" : imgs}
+
+
