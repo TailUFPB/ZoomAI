b247490825f144e5b54b2f0a06fcb08c291dfe94
Maria <mary.m0rhw@gmail.com>
2024-05-01 18:39:08 -0300

    created myprojects page

--- frontend/src/components/MyProjects/MyProjects.js
+++ frontend/src/components/MyProjects/MyProjects.js
@@ -13,7 +13,37 @@ import {FreeMode, Pagination} from 'swiper/modules';
 const MyProjects = () => {
     return (
       <div className='flex items-center justify-center flex-col h-screen bg-black'>
+        <Swiper
+          breakpoints={{
+            340: {
+              slidesPerView: 2,
+              spaceBetween: 15
+            },
+            700: {
+              slidesPerView: 3,
+              spaceBetween: 15
+            }
+          }}
 
+          freeMode={true}
+          pagination= {{
+            clickable: true
+          }}
+          
+          modules={[FreeMode, Pagination]}
+          className='max-w-[90%] lg:max-w-[80%]'
+        >
+          {/* ServiceData será a pagina com os projetos feito
+                    {ServiceData.map((item) => (
+            <SwiperSlide key={item.title}>
+              <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-x1 px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px]'>
+                
+              </div>
+            </SwiperSlide>
+          ))
+          }*/}
+
+        </Swiper>
 
       </div>
     );
