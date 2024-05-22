4fe7872abd3ed07b4c851c70bb95e77bdb449725
Franky03 <kaikym20@gmail.com>
2024-05-21 23:29:26 -0300

    adding the projects page v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+
+//layout pages
+import Navbar from './components/Navbar';
+import TextOnly from './components/TextOnly';
+import MyProjects from './components/MyProjects';
+import Footer from './components/Footer';
 import Project from './pages/Zoom';
+   
+
+const Layout= ({children}) => (
+  <>
+    <Navbar />
+    {children}
+    <Footer />
+  </>
+);
+
 
 function App() {
    return (
       <div>
          <BrowserRouter>
             <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/zoom" element={<Project />} />
+              <Route path="/" element={<Layout> <TextOnly /> </Layout>} />
+              <Route path='/projects' element={<MyProjects />} />
+              <Route path="/zoom" element={<Project />} />
             </Routes>
          </BrowserRouter>
       </div>
