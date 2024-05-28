84851a4cc76c0cd7a1a4b0d75a3130aaa7b386ff
Franky03 <kaikym20@gmail.com>
2024-05-28 20:31:00 -0300

    changing tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,13 +10,3 @@ module.exports = {
   },
   plugins: [],
 }
-
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
